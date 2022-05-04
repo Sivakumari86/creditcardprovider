70a25ba6d767f312efea6638f41bba338652cca4
sivakumari peddireddy <psivakumari86@gmail.com>
2022-05-04 17:34:18 +0100

    first commit

---
+++ src/test/java/com/creditcard/system/main/CreditcardproviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditcard.system.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditcardproviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
